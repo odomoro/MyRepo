d613642dd7cff36fcd803c74cbe4b980461d1364
Odone M da Rosa <odone@intelidata.inf.br>
2012-12-26 18:07:21 -0200

    Teste

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '98bd0d095c3090df04ce87fa42bf81b4cfa3c6a3adeb3ea4e574538afa112c222fc07cd16cfa22985395630d89038a57f260f7f79c559e102fbd100eef68ad75'
